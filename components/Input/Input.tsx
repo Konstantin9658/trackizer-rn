a48cf8ec02aeae96534df61cc5eced21b6a65fa2
k.sokolovski <k.sokolovski@mercurydevelopment.com>
2024-07-03 23:58:37 +0400

    Implement Register by email screen

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -3,25 +3,39 @@ import { inputStyles } from "./styles";
 import { InputProps } from "./Input.types";
 // import { useState } from "react";
 
-export const Input = ({ label, isPassword, isRegister = false, ...props }: InputProps) => {
-	// const [password, setPassword] = useState<string>("");
-	return (
-		<View style={inputStyles.wrapper}>
-			{label && <Text style={inputStyles.label}>{label}</Text>}
-			<TextInput secureTextEntry={isPassword} style={inputStyles.input} {...props} />
-			{isRegister && (
-				<View>
-					<View style={inputStyles.passLevel}>
-						<View style={[inputStyles.passLevelBox, inputStyles.passLevelBoxFirst]} />
-						<View style={inputStyles.passLevelBox} />
-						<View style={inputStyles.passLevelBox} />
-						<View style={[inputStyles.passLevelBox, inputStyles.passLevelBoxLast]} />
-					</View>
-					<Text style={inputStyles.passLevelDesription}>
-						Use 8 or more characters with a mix of letters, numbers&nbsp;&&nbsp;symbols.
-					</Text>
-				</View>
-			)}
-		</View>
-	);
+export const Input = ({
+  label,
+  isPassword,
+  isRegister = false,
+  ...props
+}: InputProps) => {
+  // const [password, setPassword] = useState<string>("");
+  return (
+    <View style={inputStyles.wrapper}>
+      {label && <Text style={inputStyles.label}>{label}</Text>}
+      <TextInput
+        secureTextEntry={isPassword}
+        style={inputStyles.input}
+        {...props}
+      />
+      {isRegister && (
+        <View>
+          <View style={inputStyles.passLevel}>
+            <View
+              style={[inputStyles.passLevelBox, inputStyles.passLevelBoxFirst]}
+            />
+            <View style={inputStyles.passLevelBox} />
+            <View style={inputStyles.passLevelBox} />
+            <View
+              style={[inputStyles.passLevelBox, inputStyles.passLevelBoxLast]}
+            />
+          </View>
+          <Text style={inputStyles.passLevelDesription}>
+            Use 8 or more characters with a mix of letters,
+            numbers&nbsp;&&nbsp;symbols.
+          </Text>
+        </View>
+      )}
+    </View>
+  );
 };
