cc6e9457c0dea6c251a0c9519177c840f49d98d5
Konstantin Sokolovski <k.sokolovski@mercurydevelopment.com>
2024-07-23 11:27:13 +0400

    Delete/rename files

--- app/register/index.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterScreen } from "@/screens/register/Register";
-
-export default function Register() {
-  return <RegisterScreen />;
-}
--- components/Input/Input.types.ts
+++
@@ -1,9 +0,0 @@
-import { TextInputProps } from "react-native";
-
-export interface InputProps extends TextInputProps {
-  label: string;
-  isPassword?: boolean;
-  isCenteredLabel?: boolean;
-  isRegister?: boolean;
-  isLogin?: boolean;
-}
