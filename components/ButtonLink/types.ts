a6652e7f9ae65fe0a0ad694906c89be7307c1978
Konstantin Sokolovski <k.sokolovski@mercurydevelopment.com>
2024-07-31 17:09:51 +0400

    Delete ButtonLink component and RadialComponent

--- components/ButtonLink/types.ts
+++
@@ -1,9 +0,0 @@
-import { ExpoRouter } from "expo-router/types/expo-router";
-
-export type VariantButton = "primary" | "secondary";
-
-export interface ButtonLinkProps {
-  text: string;
-  variant: VariantButton;
-  route: ExpoRouter.Href;
-}
